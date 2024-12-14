4d58b8d35cc95aacaa4f209da0216a47ded65a2b
e-orquia <orquiaerikacunanan@gmail.com>
2024-12-15 08:55:12 +1200

    Initial commit

---
+++ todolist/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from .views import todo_update
+
+urlpatterns = [
+    path('', views.todo_list, name='todo_list'),
+    path('create/', views.todo_create, name='todo_create'),
+   path('todo/<int:pk>/update/', views.todo_update, name='todo_update'),
+    path('delete/<int:pk>/', views.todo_delete, name='todo_delete'),
+]
